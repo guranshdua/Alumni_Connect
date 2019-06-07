8dc8292bb60475e15f5c73b4f866b86adacd7ccf
Guransh Dua <guransh.dua1@gmail.com>
2019-06-07 21:58:43 +0530

    Home Page

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div class="footer">
+  <div class="con">
+    <div class="row text-white">
+      <div class="col-md-3">
+        <img src=".\images\calogowhite.png" style="width:50%; height:auto;" />
+      </div>
+      <div class="col-md-3">
+        <h4 class="f600">TOOLS</h4>
+        <br />
+        <h5 class="f400">Alumni Dashboard</h5>
+        <h5 class="f400">Admin Dashboard</h5>
+      </div>
+      <div class="col-md-3">
+        <h4 class="f600">USEFUL LINKS</h4>
+        <br />
+        <h5 class="f400">Terms of Use</h5>
+        <h5 class="f400">Privacy Policy</h5>
+      </div>
+      <div class="col-md-3">
+        <h4 class="f600">CONTACT</h4>
+        <br />
+        <h5 class="f400"><i class="fas fa-home"></i> Lorem Ipsum</h5>
+        <h5 class="f400"><i class="fas fa-phone"></i>&nbsp 9876543210</h5>
+      </div>
+    </div>
+  </div>
+  <div class="copyright">
+    <p class="text-white" style="text-align: center;">
+      &copy 2019 Campus Interaction
+    </p>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -6,8 +6,42 @@
      ?>
   </head>
   <body>
+    <section id="home1">
+      <?php
+      include("./nav.php");
+       ?>
+
+       <div >
+         <h2 class="text-light-dark">MANAGE YOUR ALUMNI.<br>ALL FROM A SINGLE PORTAL.</h2>
+         <button class="btn btn-success btn-lg">REQUEST A DEMO</button>
+         <img src="./images/home_laptop.png" class="lap"></img>
+       </div>
+    </section>
+    <section id="home2">
+      <div class="row">
+        <div class="col-md-7">
+          <img src="./images/home_laptop_1.png" class="lap ml-auto mr-auto" ></img>
+        </div>
+        <div class="col-md-5">
+          <br  />
+          <h2 class="f900">WHAT DO WE OFFER?</h2>
+          <br>
+          <h4 class="f700">CUSTOMISED PLATFORM FOR ALUMNI MANAGEMENT.</h4>
+          <h4 class="f600 text-light-dark">
+            ALUMNI STATISTICS. <br />JOB MANAGEMENT. <br />ALUMNI EVENTS. <br />NEWS FEED. <br />AND MORE.
+          </h4>
+          <br /><br /><br /><br /><br />
+
+              <button class="btn btn-success btn-lg">CHECK IT OUT YOURSELF!</button>
+
+        </div>
+      </div>
+    </section>
+
     <?php
-    include("./nav.php");
+    include("footer.php");
      ?>
+
+
   </body>
 </html>
