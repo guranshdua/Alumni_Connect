47be8603531f9b138e03e3ae9a4146b823d54edc
Guransh Dua <guransh.dua1@gmail.com>
2019-06-09 12:35:10 +0530

    Events(Admin)

---
+++ admin/events.php
@@ -0,0 +1,190 @@
+<!doctype HTML>
+<html>
+  <head>
+    <?php
+    include("./partials/meta.php");
+     ?>
+  </head>
+  <body>
+    <div>
+      <?php
+      include("./partials/nav.php");
+       ?>
+       <div class="news">
+         <div class="row">
+           <div class="col-md-3" >
+             <div class="affix">
+                 <div class="card" style="width:100%; height:100%; z-index:1;">
+                  <div class="card-body">
+                    <br /><br /><br />
+                    <div class="row">
+                      <div class="col-sm-12 f700" >
+                        <img src="../images/home_laptop_1.png"  style="height:50px; border-radius:50px;"/>&nbsp&nbsp&nbspGURANSH DUA
+                      </div>
+                    </div>
+                    <div class="dropdown-divider"></div>
+                    <a href="./index.php"><h6 class="card-title f600"><i class="fas fa-home"></i> Dashboard</h6></a>
+                    <a href="./news.php"><h6 class="card-title f600"><i class="far fa-newspaper"></i> News Feed</h6></a>
+                    <a href="./events.php"><h6 class="card-title f600"><i class="fas fa-users"></i> Events</h6></a>
+                    <a href="./photo.php"><h6 class="card-title f600"><i class="fas fa-camera"></i> Photo Gallery</h6></a>
+                    <a href="./jobs.php"><h6 class="card-title f600"><i class="fas fa-chart-line"></i> Careers</h6></a>
+                    <a href="./donation.php"><h6 class="card-title f600"><i class="fas fa-rupee-sign"></i> Donations</h6></a>
+                  </div>
+                </div>
+             </div>
+           </div>
+           <div class="col" style="padding-right:30px">
+             <div class="row">
+               <div class="col-md-12">
+                 <br /><br /><br /><br />
+                 <h2 style="text-align:center;" class="f900">UPCOMING EVENTS</h2>
+                 <br />
+                 <div class='card-container'>
+                   <div class="card ml-auto mr-auto " style="width:100%;">
+                     <div class="card-body">
+                       <h6 class="f600"><a href="eventpage.php">New Delhi Alumni Meetup</a></h6>
+                       <p>VIT Delhi Alumni Association<br />
+                       Location: Cyber Hub Gurugram</p>
+                       <div class="row">
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Posted On:<br />29 May'19
+                           </p>
+                         </div>
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Register Till:<br />29 June'19
+                           </p>
+                         </div>
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Event Date:<br />29 June'19
+                           </p>
+                         </div>
+                         <div style="text-align:right" class="col">
+                           <br />
+                           <button class="btn btn-primary">View</button>
+                         </div>
+                       </div>
+
+                     </div>
+                   </div>
+                 </div>
+                 <div class='card-container'>
+                   <div class="card ml-auto mr-auto " style="width:100%;">
+                     <div class="card-body">
+                       <h6 class="f600"><a href="eventpage.php">New Delhi Alumni Meetup</a></h6>
+                       <p>VIT Delhi Alumni Association<br />
+                       Location: Cyber Hub Gurugram</p>
+                       <div class="row">
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Posted On:<br />29 May'19
+                           </p>
+                         </div>
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Register Till:<br />29 June'19
+                           </p>
+                         </div>
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Event Date:<br />29 June'19
+                           </p>
+                         </div>
+                         <div style="text-align:right" class="col">
+                           <br />
+                           <button class="btn btn-primary">View</button>
+                         </div>
+                       </div>
+
+                     </div>
+                   </div>
+                 </div>
+                 <div class='card-container'>
+                   <div class="card ml-auto mr-auto " style="width:100%;">
+                     <div class="card-body">
+                       <h6 class="f600"><a href="eventpage.php">New Delhi Alumni Meetup</a></h6>
+                       <p>VIT Delhi Alumni Association<br />
+                       Location: Cyber Hub Gurugram</p>
+                       <div class="row">
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Posted On:<br />29 May'19
+                           </p>
+                         </div>
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Register Till:<br />29 June'19
+                           </p>
+                         </div>
+                         <div class="col-md-3 col-sm-4 col-6">
+                           <p>
+                             Event Date:<br />29 June'19
+                           </p>
+                         </div>
+                         <div style="text-align:right" class="col">
+                           <br />
+                           <button class="btn btn-primary">View</button>
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <h2 style="text-align:center;" class="f900">PAST EVENTS</h2>
+            <div class="row">
+              <div class="col-md-6">
+                <div class='card-container'>
+                  <div class="card ml-auto mr-auto " style="width:100%;">
+                    <div class="card-body">
+                      <h6 class="f600"><a href="eventpage.php">New Delhi Alumni Meetup</a></h6>
+                      <p>VIT Delhi Alumni Association<br />
+                      Location: Cyber Hub Gurugram</p>
+                      <div class="row">
+                        <div class="col-md-5 col-sm-8 col-10">
+                          <p>
+                            Event Date:<br />29 June'19
+                          </p>
+                        </div>
+                        <div style="text-align:right" class="col">
+                          <br />
+                          <button class="btn btn-primary">View</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class='card-container'>
+                  <div class="card ml-auto mr-auto " style="width:100%;">
+                    <div class="card-body">
+                      <h6 class="f600"><a href="eventpage.php">New Delhi Alumni Meetup</a></h6>
+                      <p>VIT Delhi Alumni Association<br />
+                      Location: Cyber Hub Gurugram</p>
+                      <div class="row">
+                        <div class="col-md-5 col-sm-8 col-10">
+                          <p>
+                            Event Date:<br />29 June'19
+                          </p>
+                        </div>
+                        <div style="text-align:right" class="col">
+                          <br />
+                          <button class="btn btn-primary">View</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+         </div>
+
+       </div>
+
+     </div>
+   </body>
+   </html>
